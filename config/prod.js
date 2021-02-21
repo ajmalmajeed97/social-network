d4e70a947d3f72329882976f049d0206f5fc9c5d
ajmalmajeed97 <49806241+ajmalmajeed97@users.noreply.github.com>
2021-02-21 21:44:39 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports={
-    MONGOURI:'mongodb+srv://ajmal:ajmalmajeed@cluster0.wyuag.mongodb.net/social?retryWrites=true&w=majority',
-    JWT_SECRET:'ajmal',
-    SENDGRID_API:'sayoojvimal567',
-    EMAIL:process.env.EMAIL
+    MONGOURI:
+    JWT_SECRET:
+    SENDGRID_API:
+    EMAIL:
 }
